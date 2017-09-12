bd3aef24e57c2654bd279ff2c0fd958eb3c5926d
Maimit <maimit@thinkbiz.co.in>
2017-09-12 17:21:16 +0530

    add post method

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+
+// create application/json parser
+app.use(bodyParser.json());
+
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'))
 
 app.get('/', function(req, res) {
     res.render('index')
@@ -11,6 +20,9 @@ app.get('/contact', function(req, res) {
     res.render('contact')
 })
 
+app.post('/contact', function(req, res){
+    res.render('contact_success',{data: req.body})
+})
 app.get('/profile/:name', function(req, res) {
     var data = { age: 26, contact_no: +91 - 9824341494, hobbies: ['Reading', 'Rpogramming', "New Technology"] }
     res.render('profile', { person: req.params.name, data: data });
